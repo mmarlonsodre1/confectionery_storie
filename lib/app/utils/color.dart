3e64ad4cf0f932d2be9ba433466543584023c393
mmarlonsodre <mmarlonsodre@gmail.com>
2021-07-24 11:57:16 -0300

    Utils de cor e estilo de texto

---
+++ lib/app/utils/color.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+Color primaryColor = Color(0xFF3474e0);
+Color white = Color(0xFFFFFFFF);
+Color whiteBackground = Color(0xFFF5F5F5);
+Color black = Color(0xFF000000);
---
+++ lib/app/utils/text_style.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle textTitle1 = GoogleFonts.openSans(
+  fontSize: 24,
+  color: Color(0xFF303030),
+  fontWeight: FontWeight.w800
+);
+
+TextStyle textTitle2 = GoogleFonts.openSans(
+    fontSize: 22,
+    color: Color(0xFF303030),
+    fontWeight: FontWeight.w600
+);
+
+TextStyle textBody1 = GoogleFonts.openSans(
+    fontSize: 14,
+    color: Color(0xFF303030),
+    fontWeight: FontWeight.normal
+);
